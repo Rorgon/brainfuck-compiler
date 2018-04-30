de028aa5ff657f1df252c4e62dec26e8cab4d692
Sabau Florin <36925871+Rorgon@users.noreply.github.com>
2018-04-30 14:55:48 +0300

    Added settings

--- main.cpp
+++ main.cpp
@@ -43,9 +43,9 @@ int jumpb(int i,std::string input)
     return i+1;
 }
 
-void run(std::string input)
+void run(std::string input,size_t arrsize,int input_method)
 {
-    char arr[100000]={};
+    char* arr=new char[arrsize]();
     int p=0;
     for(int i=0;i<input.length();i++)
     {
@@ -65,9 +65,14 @@ void run(std::string input)
             std::cout<<arr[p];
         if(input[i]==',')
         {
-            int in;
-            std::cin>>in;
-            arr[p]=in;
+            if(input_method==1)
+            {
+                int in;
+                std::cin>>in;
+                arr[p]=in;
+            }
+            else
+                std::cin>>arr[p];
         }
     }
 }
@@ -75,7 +80,34 @@ void run(std::string input)
 int main()
 {
     std::string input;
-    read(input);
-    run(input);
+    size_t arrsize=10000; //default
+    int input_method=1; //1 for numbers and 2 for ASCII
+    std::cout<<"Type your command:\n";
+    std::cout<<"size \ninput \nrun \nexit \n";
+    while(1)
+    {
+        std::cin>>input;
+        if(input=="run")
+        {
+            input="";
+            read(input);
+            run(input,arrsize,input_method);
+            break;
+        }
+        else if(input=="size")
+        {
+            std::cout<<"Enter the size of the array:\n";
+            std::cin>>arrsize;
+        }
+        else if(input=="input")
+        {
+            std::cout<<"Enter 1 for numbers or 2 for ASCII characters:\n";
+            std::cin>>input_method;
+        }
+        else if(input=="exit")
+            break;
+        else
+            std::cout<<"Command not found\n";
+    }
     return 0;
 }
